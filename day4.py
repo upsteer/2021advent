589b8cac6e54b04623e5feebe32492653ae7b3fc
Romit Khanal <rkm@logpoint.com>
2021-12-07 10:47:55 +0545

    Day 5 Complete

--- day4.py
+++ day4.py
@@ -37,59 +37,69 @@ for li in sorted(mixed_dict.keys()):
 print("horizontal \n", boards)
 print("vertical \n", mixed_boards)
 print("draw \n", draw)
+def calculate_who_won(boards, mixed_boards, draw):
+	def max_indexer(lines):
+		index_list = []
+		num_list = []
+		win_index_list = []
+		for inn, line in enumerate(lines):
+			max_index = 0
+			max_num = 0
+			win_index = 0
+			for num in line:
+				if draw.index(num)>max_index:
+					max_index = draw.index(num)
+					max_num = num
+					win_index = inn
+			index_list.append(max_index)
+			num_list.append(max_num)
+			win_index_list.append(win_index)
+		return (min(index_list),
+			num_list[index_list.index(min(index_list))],
+			win_index_list[index_list.index(min(index_list))])
 
-def max_indexer(lines):
-	index_list = []
-	num_list = []
-	line_max = []
-	win_index_list = []
-	for inn, line in enumerate(lines):
-		max_index = 0
-		max_num = 0
-		win_index = 0
-		for num in line:
-			if draw.index(num)>max_index:
-				max_index = draw.index(num)
-				max_num = num
-				win_index = inn
-		line_max.append(f"{str(line)} + {max_num} + {max_index}")
-		index_list.append(max_index)
-		num_list.append(max_num)
-		win_index_list.append(win_index)
-	return (min(index_list),
-		num_list[index_list.index(min(index_list))],
-		win_index_list[index_list.index(min(index_list))])
+	(min_i_h, draw_num_h, win_ind_h) = max_indexer(boards)
+	(min_i_v, draw_num_v, win_ind_v) = max_indexer(mixed_boards)
+	fast_draw = draw_num_h
+	final_board = boards
+	winner_board = win_ind_h
+	if min_i_h > min_i_v:
+		fast_draw = draw_num_v
+		final_board = mixed_boards
+		winner_board = win_ind_v
+		print("won vertical")
 
-(min_i_h, draw_num_h, win_ind_h) = max_indexer(boards)
-(min_i_v, draw_num_v, win_ind_v) = max_indexer(mixed_boards)
-print('h', draw_num_h, win_ind_h)
-print('v', draw_num_v, win_ind_v)
-fast_draw = draw_num_h
-final_board = boards
-winner_board = win_ind_h
-if min_i_h > min_i_v:
-	fast_draw = draw_num_v
-	final_board = mixed_boards
-	winner_board = win_ind_v
-	print("won vertical")
+	board_num = winner_board-winner_board%5
+	winner = final_board[board_num:board_num+5]
+	return winner, fast_draw, board_num 
 
-board_num = winner_board-winner_board%5
-print(board_num)
-winner = final_board[board_num:board_num+5]
-draw = draw[0:draw.index(fast_draw)+1]
+board_won, fastest_draw, board_number = calculate_who_won(boards, mixed_boards, draw)
+draw_copy = draw[0:draw.index(fastest_draw)+1]
 sum=0
 untouched = []
-print(winner)
-for line in winner:
+for line in board_won:
 	for num in line:
-		if not num in draw:
+		if not num in draw_copy:
 			sum+=num
 			untouched.append(num)
-print('un', untouched)
-print(sum, fast_draw, sum*fast_draw)
+print('Part 1', sum, fastest_draw, sum*fastest_draw)
 
-
-
+print('won', board_number)
+while len(boards) > 5:
+	del boards[board_number:board_number+5]
+	del mixed_boards[board_number:board_number+5]
+	print(boards)
+	print(mixed_boards)
+	board_won, fastest_draw, board_number = calculate_who_won(boards, mixed_boards, draw)
+	draw_copied = draw[0:draw.index(fastest_draw)+1]
+	sum=0
+	untouched = []
+	for line in board_won:
+		for num in line:
+			if not num in draw_copied:
+				sum+=num
+				untouched.append(num)
+	print(sum, fastest_draw, sum*fastest_draw)
 
 
 
---
+++ day5.py
@@ -0,0 +1,74 @@
+import sys
+
+file = ""
+if str(sys.argv[len(sys.argv)-1]) == 'test':
+	file = "tests/"
+data_file = file + "day5.txt"
+hash_dict = {}
+with open(data_file, 'r') as f:
+	for line in f:
+		a,b = line.strip().replace(" ", "").split("->")
+		start_x, start_y = a.split(',')
+		end_x, end_y = b.split(',')
+		diff_x = abs(int(end_x) - int(start_x))
+		diff_y = abs(int(end_y) - int(start_y))
+		diff = diff_x or diff_y
+		diff_dir = "x"
+		if diff_x and diff_y:
+			# print(f"{start_x}, {start_y}")
+			# print("to")
+			# print(f"{end_x}, {end_y}\n")
+			for i in range(diff+1):
+				end_x_temp = int(start_x)
+				end_y_temp = int(start_y)
+				if end_x_temp - int(end_x) == end_y_temp - int(end_y):
+					if int(end_x) > int(start_x):
+						end_x_temp += i
+						end_y_temp += i
+					else:
+						end_x_temp -= i
+						end_y_temp -= i
+				elif int(end_x) > int(start_x):
+					end_x_temp += i
+					end_y_temp -= i
+				else:
+					end_x_temp -= i
+					end_y_temp += i
+				# print("---")
+				# print(end_x_temp, end_y_temp)
+				# print("---")
+				if hash_dict.get(f"{end_x_temp},{end_y_temp}"):
+					hash_dict[f"{end_x_temp},{end_y_temp}"] += 1
+				else:
+					hash_dict[f"{end_x_temp},{end_y_temp}"] = 1
+			continue
+		if not diff_x:
+			diff_dir = "y"
+		for i in range(diff+1):
+			index = i
+			if diff_dir == "x":
+				if int(end_x) > int(start_x):
+					index+=int(start_x)
+				else:
+					index+=int(end_x)
+				if hash_dict.get(f"{index},{end_y}"):
+					hash_dict[f"{index},{end_y}"] += 1
+				else:
+					hash_dict[f"{index},{end_y}"] = 1
+			else:
+				if int(end_y) > int(start_y):
+					index+=int(start_y)
+				else:
+					index+=int(end_y)
+				if hash_dict.get(f"{end_x},{index}"):
+					hash_dict[f"{end_x},{index}"] += 1
+				else:
+					hash_dict[f"{end_x},{index}"] = 1
+count = 0
+for val in hash_dict:
+	if hash_dict[val] >= 2:
+		count+=1
+print(count)
+
+
+# For Part 1 comment out line 21 to 43
