b545a37d5bb9ec33af4a653d4d0f8da88198575d
Romit Khanal <rkm@logpoint.com>
2021-12-27 18:54:54 +0545

    Day 6 Complete

--- day6.py
+++ day6.py
@@ -18,13 +18,16 @@ for inp in input:
 	else:
 		count_arr[inp]=1
 
-for i in range(256):
-	at_zero = count_arr.pop(0)
-	if at_zero != 0:
-		count_arr.append(at_zero)
-		count_arr[6]+=at_zero
-	else:
-		count_arr.append(0)
+def run_simulation(days):
+	for i in range(days):
+		at_zero = count_arr.pop(0)
+		if at_zero != 0:
+			count_arr.append(at_zero)
+			count_arr[6]+=at_zero
+		else:
+			count_arr.append(0)
+	return sum(count_arr)
 
-print(sum(count_arr))
+print("Part 1", run_simulation(80))
+print("Part 2", run_simulation(256-80))
 
