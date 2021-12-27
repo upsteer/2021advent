b92d9191a5c793349c9cbe72d76de4b7ac5b1833
Romit Khanal <rkm@logpoint.com>
2021-12-27 18:50:47 +0545

    Day 6 Complete

--- day6.py
+++ day6.py
@@ -9,31 +9,22 @@ with open(data_file, 'r') as f:
     input = f.read()
     input = [int(inp) for inp in input.split(',')]
 
-day_list = []
-each_day = input
-population = len(input)
-for day in range(80):
-	new_day = []
-	add_new = 0
-	for inp in each_day:
-		if inp == 0:
-			add_new += 1
-			new_day.append(6)
-		num = inp-1
-		if num >= 0:
-			new_day.append(num)
-	for i in range(add_new):
-		new_day.append(8)
-	each_day = new_day
-	print(len(each_day))
 
-population = len(input)
+count_arr = [0,0,0,0,0,0,0,0,0]
 
-print(population)
-total_days = 80
-for fish in each_day:
-	temp_day = total_days - fish
-	factor_8 = math.floor(temp_day/8)
-	population += math.floor(math.pow(2, factor_8))
+for inp in input:
+	if count_arr[inp]:
+		count_arr[inp]+=1
+	else:
+		count_arr[inp]=1
 
-print(population*2)
+for i in range(256):
+	at_zero = count_arr.pop(0)
+	if at_zero != 0:
+		count_arr.append(at_zero)
+		count_arr[6]+=at_zero
+	else:
+		count_arr.append(0)
+
+print(sum(count_arr))
+
